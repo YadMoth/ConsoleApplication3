3d5e3fc3be1c3d3a51a29682a0c0f6db3ceecf24
Amandykov-Batyr <batyramandykov2@gmail.com>
2025-04-04 16:27:55 +0300

    Update ConsoleApplication3.cpp

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,20 +1,60 @@
-﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+int main() {
+    ContactManager manager;
+    int choice;
+    std::string name, phone, email;
 
-#include <iostream>
+    do {
+        std::cout << "1. Add Contact\n";
+        std::cout << "2. Delete Contact\n";
+        std::cout << "3. Find Contact\n";
+        std::cout << "4. Change Contact\n";
+        std::cout << "5. Save to File\n";
+        std::cout << "6. Display Contacts\n";
+        std::cout << "0. Exit\n";
+        std::cout << "Choose an option: ";
+        std::cin >> choice;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+        switch (choice) {
+            case 1:
+                std::cout << "Enter name, phone, email: ";
+                std::cin >> name >> phone >> email;
+                manager.addContact(name, phone, email);
+                break;
+            case 2:
+                std::cout << "Enter name to delete: ";
+                std::cin >> name;
+                manager.deleteContact(name);
+                break;
+            case 3:
+                std::cout << "Enter name to find: ";
+                std::cin >> name;
+                if (Contact* contact = manager.findContact(name)) {
+                    contact->display();
+                } else {
+                    std::cout << "Contact not found.\n";
+                }
+                break;
+            case 4:
+                std::cout << "Enter name to change: ";
+                std::cin >> name;
+                std::cout << "Enter new phone and email: ";
+                std::cin >> phone >> email;
+                manager.changeContact(name, phone, email);
+                break;
+            case 5:
+                manager.saveToFile("contacts.txt");
+                std::cout << "Contacts saved to file.\n";
+                break;
+            case 6:
+                manager.displayContacts();
+                break;
+            case 0:
+                std::cout << "Exiting...\n";
+                break;
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 0);
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
